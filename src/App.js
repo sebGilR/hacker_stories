a6c5e3564eaf03185ac3233cb9d06f0908caeabe
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-06-30 11:00:45 -0500

    Basic project structure

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function getTitle(title) {
+  return title
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' />
+    </div>
+  );
+}
+
+export default App;
