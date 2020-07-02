3aeff12b0ad36aaf64d4ecdbdfa9decf8e57ac32
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-02 11:44:45 -0500

    Wrap elements in fragment

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const Search = ({ onSearch, search }) => (
-  <div>
+  <>
     <label htmlFor="search">Search: </label>
     <input id="search" type="text" onChange={onSearch} value={search} />
-  </div>
+  </>
 )
 
 export default Search;
