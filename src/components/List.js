5507cb24a79fcb19354561438baaa5756c51cc2d
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-01 21:03:36 -0500

    Refactor to use rest and spread syntax

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,6 @@ import React from 'react';
 import Item from './Item';
 
 const List = ({ list }) =>
-  list.map(item => <Item key={item.objectID} item={item} />);
+  list.map(({ objectID, ...item }) => <Item key={objectID} {...item} />);
 
 export default List;
