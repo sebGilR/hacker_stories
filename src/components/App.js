ea400dfbfa50fa9213d164233c7459d90ca4b616
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-07-02 12:28:20 -0500

    Add default input type

--- src/components/App.js
+++ src/components/App.js
@@ -54,7 +54,6 @@ const App = () => {
         label="Search"
         value={searchTerm}
         onInputChange={handleSearch}
-        type="text"
       />
 
       <hr />
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const InputWithLabel = ({ id, label, value, onInputChange, type }) => (
+const InputWithLabel = ({ id, label, value, onInputChange, type = 'text' }) => (
   <>
     <label htmlFor={id}>{label}: </label>
     <input id={id} type={type} onChange={onInputChange} value={value} />
